2630f03ef223cd7b58c84578b1990cc21782b6a3
Felix <git@studio.elasticizer.dev>
2024-05-21 00:34:06 +0800

    0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
 	content: [
-		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./node_modules/preline/preline.js'
 	],
 
